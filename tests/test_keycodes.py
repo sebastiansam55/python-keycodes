ddc7a50e350f872462478842a01859800fb5d588
Sam Sebastian <sebastiansam55@gmail.com>
2021-04-01 15:17:41 -0400

    Update tests

--- tests/test_keycodes.py
+++ tests/test_keycodes.py
@@ -35,6 +35,15 @@ def test_item_to_evdev(in_put, out_put):
 def test_list_to_evdev(items, result):
     assert result == keycodes.list_to_evdev(items)
 
-def test_str_to_evdev():
-    pass
+@pytest.mark.parametrize("string, result",
+    [
+    ("this", [20, 35, 23, 31]),
+    ("IS", [[42, 23], [42, 31]]),
+    ("6 or more", [7, 57, 24, 19, 57, 50, 24, 19, 18]),
+    ("tests", [20, 18, 31, 20, 31]),
+    ("of the", [24, 33, 57, 20, 35, 18]),
+    ("module", [50, 24, 32, 22, 38, 18]),
+])
+def test_str_to_evdev(string, result):
+    assert result == keycodes.str_to_evdev(string)
 
